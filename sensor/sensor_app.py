690802e580ccee70cf6c2fb140037382665f3977
pooja2701 <110535625+pooja2701@users.noreply.github.com>
2022-08-09 19:09:03 +0530

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -25,8 +25,16 @@ recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("House sensor records for date {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 
 # Module 3 code here:
+temperature_data = TemperatureData(data)
+recs = temperature_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Temperature sensor records for area {} = {}".format(test_area, len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 # Module 4 code here:
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+print("\nHouse Temperature sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recs)))
+print("\tMaximum: {0}, Minimum: {1} temperatures".format(max(recs), min(recs)))
 
 # Module 5 code here:
 
